ae6e3ff46595dca7a47cfd294f5d04f71d9d1b56
masayume <masayume.blog@gmail.com>
2020-04-18 23:21:13 +0200

    handling caching events + add reference via nuget

--- 02-files/FileProgram.cs
+++ 02-files/FileProgram.cs
@@ -1,20 +1,37 @@
 using System;
 using static System.Console;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Caching;
+
+// to install System.Runtime.Caching 
+// 1 - install Nuget Package Manager
+//   CTRL+P -> ext install jmrog.vscode-nuget-package-manager
+// 2 - press F1, search: nuget package manager: add package
+//   enter System.Runtime.Caching and proceed
+// 3 - restore from terminal
+// dotnet restore
+// 4 - add reference at build time: -reference:System.Runtime.Caching.dll
+// csc -t:exe -define:DEBUG -reference:System.Runtime.Caching.dll -out:files.exe FileProgram.cs FileProcessor.cs
 
 // DIR: ~/DATA/E/Temp/demon/unity/dotNET/02-files/
-// mono files.exe --file in/data1.txt
-// mono files.exe --dir in TEXT
+// mono files.exe <DIRECTORY>
+// mono files.exe in
 
 
 namespace DataProcessor
 {
     class FileProgram
     {
+        // avoid duplicate file systemevents
+        // private static ConcurrentDictionary<string, string> FilesToProcess = new ConcurrentDictionary<string, string>();
+        
+        private static MemoryCache filesToProcess = MemoryCache.Default;
+
         static void Main(string[] args)
         {
             WriteLine("Parsing command line options");
@@ -32,13 +49,14 @@ namespace DataProcessor
                 WriteLine($"Watching directory {directoryToWatch} for changes.");
 
                 using (var inputFileWatcher = new FileSystemWatcher(directoryToWatch))
+
                 {
                     inputFileWatcher.IncludeSubdirectories = false;
                     // inputFileWatcher.InternalBufferSize = 32768; // 32 Kb
                     inputFileWatcher.Filter = "*.*";
 
                     // inputFileWatcher.NotifyFilter = NotifyFilters.LastWrite; // events only when written
-                    inputFileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite; // events only when written
+                    inputFileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite; // catch certain events
 
                     // Events handler methods
                     inputFileWatcher.Created += FileCreated;
@@ -58,11 +76,23 @@ namespace DataProcessor
         private static void FileCreated(object sender, FileSystemEventArgs e)
         {
             WriteLine($"* File created: {e.Name} - type: {e.ChangeType}");
+
+            // these cause duplicate events
+            // var fileProcessor = new FileProcessor(e.FullPath);
+            // fileProcessor.Process();
+
+            AddToCache(e.FullPath);
         }
 
         private static void FileChanged(object sender, FileSystemEventArgs e)
         {
             WriteLine($"* File changed: {e.Name} - type: {e.ChangeType}");
+
+            // these cause duplicate events
+            // var fileProcessor = new FileProcessor(e.FullPath);
+            // fileProcessor.Process();
+
+            AddToCache(e.FullPath);
         }
 
         private static void FileDeleted(object sender, FileSystemEventArgs e)
@@ -80,6 +110,36 @@ namespace DataProcessor
             WriteLine($"ERROR: file system watching may no longer be active: {e.GetException()}");
         }
 
+
+        private static void AddToCache(string fullPath)
+        {
+            var item = new CacheItem(fullPath, fullPath);
+
+            var policy = new CacheItemPolicy
+            {
+                RemovedCallback = ProcessFile,
+                SlidingExpiration = TimeSpan.FromSeconds(2),
+            };
+
+            filesToProcess.Add(item, policy);
+        }
+
+        private static void ProcessFile(CacheEntryRemovedArguments args)
+        {
+            WriteLine($"* Cache item removed: {args.CacheItem.Key} because {args.RemovedReason}");
+
+            // check in object is expired
+            if (args.RemovedReason == CacheEntryRemovedReason.Expired)
+            {
+                var fileProcessor = new FileProcessor(args.CacheItem.Key);
+                fileProcessor.Process();
+            }
+            else
+            {
+                WriteLine($"WARNING: {args.CacheItem.Key} was removed unexpectedly and may not be available");
+            }
+
+        }
 /*
         private static void ProcessSingleFile(string filePath )
         {
--- README.md
+++ README.md
@@ -9,4 +9,13 @@ dotnet new console -o 01-files
 ## compile classes
 csc -t:exe -define:DEBUG -out:files.exe FileProgram.cs FileProcessor.cs
 
-
+## install references
+ to install System.Runtime.Caching 
+ 1 - install Nuget Package Manager
+   CTRL+P -> ext install jmrog.vscode-nuget-package-manager
+ 2 - press F1, search: nuget package manager: add package
+   enter System.Runtime.Caching and proceed
+ 3 - restore from terminal
+ dotnet restore
+ 4 - add reference at build time: -reference:System.Runtime.Caching.dll
+ csc -t:exe -define:DEBUG -reference:System.Runtime.Caching.dll -out:files.exe FileProgram.cs FileProcessor.cs
