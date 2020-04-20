a86c7718330a9392cc5271dd872819150b29661a
masayume <masayume.blog@gmail.com>
2020-04-21 01:42:54 +0200

    streamReader/Writer code

--- 04-files/BinaryFileProcessor.cs
+++ 04-files/BinaryFileProcessor.cs
@@ -17,14 +17,55 @@ namespace DataProcessor
 
         public void Process()
         {
-            byte[] data = File.ReadAllBytes(InputFilePath);
-            byte largest = data.Max();  // System.Linq
 
-            byte[] newData = new byte[data.Length + 1];
-            Array.Copy(data, newData, data.Length);
-            newData[newData.Length - 1] = largest;
+            // fileStream.Position specifies the position in the fileStream: 05 0F FF 5E 2A 00
+            // fileStream.Position = 2;
+            // int thirdByte = fileStream.ReadByte(); // FF
 
-            File.WriteAllBytes(OutputFilePath, newData);
+            // fileStream.Seek(2, SeekOrigin.Begin); // 2 is the offset; there is SeekOrigin.Current, 
+            // thirdByte = fileStream.ReadByte(); // FF
+            // fileStream.Seek(-3, SeekOrigin.End); // -3 is the offset; there is SeekOrigin.End as well
+            // threeFromEnd = fileStream.ReadByte(); // 5E
+
+            // Stream.CanSeek to know if the Stream supports random access. Not all do.
+
+            using (FileStream inputFileStream = File.Open(InputFilePath, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binaryStreamReader = new BinaryReader(inputFileStream))
+            using (FileStream outputFileStream = File.Create(OutputFilePath))
+            using (BinaryWriter binaryStreamWriter = new BinaryWriter(outputFileStream))
+            {
+                byte largest = 0;
+
+                while (binaryStreamReader.BaseStream.Position < binaryStreamReader.BaseStream.Length)
+                {
+                    byte currentByte = binaryStreamReader.ReadByte();
+                    binaryStreamWriter.Write(currentByte);
+
+                    if (currentByte > largest)
+                    {
+                        largest = currentByte;
+                    }
+                }
+
+                binaryStreamWriter.Write(largest);
+
+            }
+
         }
+
+        // MemoryStream overview
+        // using (var MemoryStream = new MemoryStream())
+        // using (var MemoryStreamWriter = new StreamWriter(memoryStream))
+        // using (var fileStream = new FileStream(@"C:\data.txt", FileMode.Create))
+        // {
+        //      memoryStreamWriter.WriteLine("Line 1");       
+        //      memoryStreamWriter.WriteLine("Line 2");       
+        //      
+        //      // Ensure everything is written to memory srtream
+        //      memoryStreamWriter.Flush();   
+        //      memoryStream.WriteTo(fileStream);   
+        // }
+
+
     }
 }
